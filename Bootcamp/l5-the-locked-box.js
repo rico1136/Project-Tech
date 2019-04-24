6d7f541d2839f93a7f9bf52c2a7b92550682ef8c
Rico Zethof <Ricozethof@gmail.com>
2019-04-24 13:54:28 +0200

    Bootcamp --> locked box finished

---
+++ Bootcamp/l5-the-locked-box.js
@@ -0,0 +1,39 @@
+// Assignment code
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;  },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    // My code
+    const locked = box.locked;
+    box.unlock();
+    try {
+        body();
+    } finally {
+        if (locked){
+            box.lock();
+        }
+    }
+}
+
+// Assignment code
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch (e) {
+    console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
